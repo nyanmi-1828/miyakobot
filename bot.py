c131bb457bc05bde0f91dd810785083daab66e7b
nyanmi-1828 <42661748+nyanmi-1828@users.noreply.github.com>
2020-05-18 09:42:50 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(activity=discord.Game(name="!helpでヘルプが見れるの　めんどくさいから一回で覚えろなの"))
     
 @bot.command(name='talk')
 async def hello(ctx):
