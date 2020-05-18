3347eebbf0f6916878b2afc41898f547d0c7aca2
nyanmi-1828 <42661748+nyanmi-1828@users.noreply.github.com>
2020-05-18 23:29:06 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ bot.remove_command('help')
 DEVELOPER_ID = '711371885300023356'
 TOKEN = 'NzExMzcxODg1MzAwMDIzMzU2.XsDv8A.l5piPx0hMkHJCF3sz62JN7SFkdM'
 
+# purin_value宣言しとかないとバグる気がする
+purin_value = 0
+
 # imgフォルダに入ってる画像のパスを全部取得
 image_list = glob.glob('img/*')
 recipe_list = []
@@ -17,6 +20,27 @@ recipe_list.append('https://www.kurashiru.com/recipes/33a876db-a731-46fb-a9bb-50
 recipe_list.append('https://www.youtube.com/watch?v=3jI-PRAZ4Y8')
 recipe_list.append('https://www.youtube.com/watch?v=m_g6jePKNTQ')
 recipe_list.append('https://www.youtube.com/watch?v=iFe-YsJ8Ts0')
+
+# おみくじ一覧
+omikuji_list = []
+omikuji_list.append("姫吉なの　プリンパーティーなの～♪")
+omikuji_list.append("大吉なの　プリンが美味しい一日になるの")
+omikuji_list.append("中吉なの　せっかくいい運勢なんだからプリンおごってなの")
+omikuji_list.append("小吉なの　プリンが食べられそうな一日なの　早くよこすの")
+omikuji_list.append("吉なの　よりよい運勢のためにミヤコにプリンをお供えするの")
+omikuji_list.append("末吉なの　いいからプリンをよこすの")
+omikuji_list.append("凶なの　ミヤコにプリンをあげないと呪われるの")
+omikuji_list.append("大凶なの　死ねなの")
+
+# 喋る言葉一覧
+talk_list = []
+talk_list.append("ウィーン。プリンを食べたらプリンを食べるパワーがみなぎってきたの。早くよこすの")
+talk_list.append("ぱくっ。ん～♪なめらかなの～♪")
+talk_list.append("やっぱりプッチンプリンは良いの。味もコンセプトも良いの。しかも3個入りなの。ぷっち～んなの～♪")
+talk_list.append("牛乳プリンは甘過ぎなくてデザートにぴったりなの～♪主食？もちろん主食もプリンなの～♪")
+talk_list.append("森永牛乳プリンのパッケージに描かれてるマスコットキャラ、「ホモちゃん」って言うらしいの…")
+talk_list.append("焼プリンは表面のガリガリが美味しいの～♪")
+talk_list.append("かぼちゃプリン？邪道なの…")
 
 @bot.event 
 async def on_ready():
@@ -50,12 +74,22 @@ async def miyako(ctx):
 async def pudding(ctx):
     purin = random.choice(recipe_list)
     await ctx.send(purin)
+    
+@bot.command()
+async def omikuji(ctx):
+    omi = random.choice(omikuji_list)
+    await ctx.send(omikuji)
 
 @bot.event
 async def on_reaction_add(reaction,user):
     print("emoji")
     print(reaction.emoji)
-    if reaction.emoji == "🍮":
-        await reaction.message.channel.send('ウィーン。プリンを食べたらプリンを食べるパワーがみなぎってきたの。早くよこすの')
+    miya_talk = random.choice(talk_list)
+    if reaction.emoji == "🍮" and purin_value < 10:
+        purin_value += 1
+        await reaction.message.channel.send(miya_talk)
+        elif purin_value == 10:
+            await ctx.send("こんなにプリンを食べたらミヤコ死んじゃうの…あ、もう死んでたの")
+            purin_value = 0
 
 bot.run(TOKEN)
