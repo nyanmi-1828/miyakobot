a12f731b7e0e25936bb4dd7ce41b01c9a020fc5f
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-23 03:46:33 +0900

    fixed bug

--- cogs/miyakor18.py
+++ cogs/miyakor18.py
@@ -7,7 +7,7 @@ class MiyakoR18(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def miyako-nsfw-r18(self,ctx):
+    async def miyakor18(self,ctx):
     with open('./src/r18.txt', mode='r', encoding='utf-8') as r18:
         miyakor18_list = r18.read().split('\n')
     miyakor18 = random.choice(miyakor18_list)
