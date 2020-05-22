9ce43ccce18a894d3470e5f32f5a38b6939f7dbf
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-22 11:59:51 +0900

    fixed bug

--- cogs/miyako.py
+++ cogs/miyako.py
@@ -9,7 +9,7 @@ class Miyako(commands.Cog):
     
     @commands.command()
     async def miyako(self,ctx):
-        image_list = glob.glob('../img/*')
+        image_list = glob.glob('./img/*')
         img = random.choice(image_list)
         await ctx.send(file=discord.File(img))
 
