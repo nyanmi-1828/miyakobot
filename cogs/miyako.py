08688fb71b30e72b566683f73b1915d6fbbc4d1c
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-23 03:06:54 +0900

    changed miyako command

--- cogs/miyako.py
+++ cogs/miyako.py
@@ -3,16 +3,26 @@ from discord.ext import commands
 import random
 import glob
 
+with open('src/r18.txt', mode='r', encoding='utf-8') as r18:
+    miyakor18_list = r18.read().split('\n')
+
 class Miyako(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.group()
     async def miyako(self,ctx):
-        image_list = glob.glob('./img/*')
-        img = random.choice(image_list)
-        await ctx.send(file=discord.File(img))
-    
+        if ctx.invoked_subcommand is None:
+            image_list = glob.glob('./img/*')
+            img = random.choice(image_list)
+            await ctx.send(file=discord.File(img))
+
+    @miyako.command()
+    async def nsfw-r18(self,ctx):
+        global miyakor18_list
+        miyakor18 = random.choice(miyakor18_list)
+        await ctx.send(miyakor18)
+
     @commands.command()
     async def talk(self,ctx):
         await ctx.send("プリンも無いのに動くわけないの")
@@ -20,6 +30,8 @@ class Miyako(commands.Cog):
     @commands.command()
     async def joubutsu(self,ctx):
         await ctx.send("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ") 
+        
+    
 
 def setup(bot):
     bot.add_cog(Miyako(bot))
