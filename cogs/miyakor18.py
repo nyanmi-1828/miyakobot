4a8be2a12426c97a7c3e23bbb43ce0476dc94ea4
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-23 03:46:53 +0900

    fixed bug

--- cogs/miyakor18.py
+++ cogs/miyakor18.py
@@ -2,16 +2,18 @@ import discord
 from discord.ext import commands
 import random
 
+with open('./src/r18.txt', mode='r', encoding='utf-8') as r18:
+    miyakor18_list = r18.read().split('\n')
+
 class MiyakoR18(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command()
     async def miyakor18(self,ctx):
-    with open('./src/r18.txt', mode='r', encoding='utf-8') as r18:
-        miyakor18_list = r18.read().split('\n')
-    miyakor18 = random.choice(miyakor18_list)
-    await ctx.send(miyakor18)
+        global miyakor18_list
+        miyakor18_link = random.choice(miyakor18_list)
+        await ctx.send(miyakor18_link)
 
 def setup(bot):
     bot.add_cog(MiyakoR18(bot))
