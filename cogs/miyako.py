53c259f9860330e2009d4199cb232258bb415de2
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-23 03:23:59 +0900

    changed miyako command

--- cogs/miyako.py
+++ cogs/miyako.py
@@ -3,9 +3,6 @@ from discord.ext import commands
 import random
 import glob
 
-with open('./src/r18.txt', mode='r', encoding='utf-8') as r18:
-    miyakor18_list = r18.read().split('\n')
-
 class Miyako(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -18,7 +15,8 @@ class Miyako(commands.Cog):
 
     @commands.command()
     async def miyako-nsfw-r18(self,ctx):
-        global miyakor18_list
+        with open('./src/r18.txt', mode='r', encoding='utf-8') as r18:
+            miyakor18_list = r18.read().split('\n')
         miyakor18 = random.choice(miyakor18_list)
         await ctx.send(miyakor18)
 
