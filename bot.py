f4470aa70a7ae9663bcc5692eac26d04a40c22b3
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-24 11:17:55 +0900

    fixed bug

--- bot.py
+++ bot.py
@@ -10,8 +10,6 @@ import aiohttp
 
 bot = commands.Bot(command_prefix='m!',help_command=None)
 BOT_TOKEN = os.environ['TOKEN']
-if not discord.opus.is_loaded(): 
-    discord.opus.load_opus("heroku-buildpack-libopus")
 purin_value = 0
 cogs = [
     'cogs.help',
@@ -112,54 +110,6 @@ async def on_reaction_add(reaction,user):
         purin_value = 0
     else:
         pass
-
-@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
-async def join(ctx):
-    """Botをボイスチャンネルに入室させます。"""
-    voice_state = ctx.author.voice
-
-    if (not voice_state) or (not voice_state.channel):
-        await ctx.send("先にオマエが入れなの")
-        return
-
-    channel = voice_state.channel
-
-    await channel.connect()
-    print("connected to:",channel.name)
-
-
-@bot.command(aliases=["disconnect","bye"])
-async def leave(ctx):
-    """Botをボイスチャンネルから切断します。"""
-    voice_client = ctx.message.guild.voice_client
-
-    if not voice_client:
-        await ctx.send("ボイスチャンネルにミヤコは入ってないの")
-        return
-
-    await voice_client.disconnect()
-    await ctx.send("ボイスチャンネルから切断したの")
-
-
-@bot.command()
-async def play(ctx):
-    """指定された音声ファイルを流します。"""
-    voice_client = ctx.message.guild.voice_client
-
-    if not voice_client:
-        await ctx.send("ボイスチャンネルにミヤコは入ってないの")
-        return
-
-    if not ctx.message.attachments:
-        await ctx.send("ファイルが添付されてないの")
-        return
-
-    await ctx.message.attachments[0].save("tmp.mp3")
-
-    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
-    voice_client.play(ffmpeg_audio_source)
-
-    await ctx.send("再生したの")
 
 @bot.event
 async def on_command_error(ctx, error):
