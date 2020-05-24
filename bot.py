d88eb5729b8de972e757bd8c4a939b67d7b3b9af
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-24 11:05:54 +0900

    fixed bug

--- bot.py
+++ bot.py
@@ -113,6 +113,54 @@ async def on_reaction_add(reaction,user):
     else:
         pass
 
+@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    voice_state = ctx.author.voice
+
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("先にオマエが入れなの")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    print("connected to:",channel.name)
+
+
+@bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """Botをボイスチャンネルから切断します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("ボイスチャンネルにミヤコは入ってないの")
+        return
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断したの")
+
+
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("ボイスチャンネルにミヤコは入ってないの")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されてないの")
+        return
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生したの")
+
 @bot.event
 async def on_command_error(ctx, error):
     ch = 713459691153391707
