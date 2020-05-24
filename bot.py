9a1d023a500035431c93f1dd7086e24415cd8339
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-24 22:17:14 +0900

    色々と変更

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@ from discord.ext import commands,tasks
 import discord
 import traceback
 import random
-import glob
 import os
 from discord.ext import commands
 import io
 import aiohttp
+import ffmpeg
+import asyncio
+
 
 bot = commands.Bot(command_prefix='m!',help_command=None)
 BOT_TOKEN = os.environ['TOKEN']
@@ -110,6 +112,54 @@ async def on_reaction_add(reaction,user):
         purin_value = 0
     else:
         pass
+
+@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    voice_state = ctx.author.voice
+
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("オマエが先にボイスチャンネルに入っている必要があるの")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    print("connected to:",channel.name)
+
+
+@bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """Botをボイスチャンネルから切断します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("ミヤコはこのサーバーのボイスチャンネルに参加してないの")
+        return
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断したの")
+
+
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("ミヤコはこのサーバーのボイスチャンネルに参加してないの")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されてないの")
+        return
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生したの")
 
 @bot.event
 async def on_command_error(ctx, error):
