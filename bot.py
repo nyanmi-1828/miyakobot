880565ccb4db099e4dcb318abc3d15340c258119
nyanmi-1828 <mikan_1828@ezweb.ne.jp>
2020-05-26 16:13:51 +0900

    youtube_dl test

--- bot.py
+++ bot.py
@@ -7,9 +7,10 @@ from discord.ext import commands
 import io
 import aiohttp
 import asyncio
+import youtube_dl
 
 bot = commands.Bot(command_prefix='m!',help_command=None)
-#BOT_TOKEN = os.environ['TOKEN']
+BOT_TOKEN = os.environ['TOKEN']
 purin_value = 0
 cogs = [
     'cogs.help',
@@ -19,6 +20,47 @@ cogs = [
 # cogs.help = helpコマンド
 # cogs.miyako = miyako,talk,joubutsuなど細かいコマンド
 # cogs.slot = slotコマンド
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 for cog in cogs:
     try:
@@ -146,8 +188,7 @@ async def play(ctx, youtube_url):
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
     channel = voice_state.channel
-    voice_channel = ctx.message.author.voice.channel
-    voice = await bot.join.channel(voice_channel)
+    url = ctx.message.content.replace("m!play","")
     print(youtube_url)
 
     # botがボイスチャンネルに接続していない場合
@@ -169,9 +210,8 @@ async def play(ctx, youtube_url):
 
     # URL先をDLして再生
     if not youtube_url == "":
-        player = await voice.create_ytdl_player(youtube_url)
-        player.start()
-        await ctx.send("再生したの")
+        player = await YTDLSource.from_url(url, loop=bot.loop)
+        ctx.voice_client.play(player)
         return
 
     if not ctx.message.attachments:
@@ -179,16 +219,6 @@ async def play(ctx, youtube_url):
         return
     else:
         return
-
-@bot.command()
-async def stop(ctx):
-    if(player.is_playing()):
-        player.stop()
-        return
-    else:
-        await ctx.send("何も再生してないの")
-        return
-    
 
 @bot.event
 async def on_command_error(ctx, error):
