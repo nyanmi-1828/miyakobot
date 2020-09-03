5b2b2aa75c553ef678f4405bc6edc82f1584c04f
st20 <st20@funagisystem.local>
2020-09-03 13:21:26 +0900

    bug fix

--- cogs/slot.py
+++ cogs/slot.py
@@ -2,35 +2,35 @@ import discord
 from discord.ext import commands
 import random
 
-slot_list = [':custard:', ':poultry_leg:', ':lemon:', ':salt:', ':candy:',':bowl_with_spoon:',':sake:']
-result_list = [
-    "プリンが3つ揃ったの～♪プッチンプリンを買ってくるの！ちょうど3つなの！",
-    "お肉が3つ揃ったの あのお姫様にあげてくるの！お礼はプリンでよろしくなの～",
-    "レモンが3つ揃ったの 恵みの一滴なの",
-    "塩が3つ揃ったの やめるの！塩をミヤコに振らないでなの～！成仏しちゃうの～！",
-    "アメが3つ揃ったの あめちゃんなの",
-    "ボウルとスプーンが3つ揃ったの これでプリンを作るの！3つ同時平行で大量生産なの～",
-    "お酒が3つ揃ったの お酒が入ったお風呂は身を清められるらしいの… や、やめてなの！ミヤコを突っ込まないで欲しいの！"
-    ]
+
 
 class Casino(commands.Cog):
     def __init__(self,bot):
-        self.bot = bot  
+        self.bot = bot
     
     @commands.command()
-    async def slot(self,ctx):
-        global slot_list
-        global result_list
-        A = random.choice(slot_list)
-        B = random.choice(slot_list)
-        C = random.choice(slot_list)
-        result = "%s%s%s" % (A, B, C)
-        if A == B and B == C:
-            await ctx.send(result + "\n" + result_list[slot_list.index(A)])
-        elif A == B or B == C or A == C:
-            await ctx.send(result + "\n" + "もうちょっとだったの！もう一回なの！")
-        else:
-            await ctx.send(result + "\n" + "はずれなの！もう一回なの！")
+    async def slot(self,ctx,kaisuu=1):
+        slot_list = [':custard:', ':poultry_leg:', ':lemon:', ':salt:', ':candy:',':bowl_with_spoon:',':sake:']
+        result_list = [
+            "プリンが3つ揃ったの～♪プッチンプリンを買ってくるの！ちょうど3つなの！",
+            "お肉が3つ揃ったの あのお姫様にあげてくるの！お礼はプリンでよろしくなの～",
+            "レモンが3つ揃ったの 恵みの一滴なの",
+            "塩が3つ揃ったの やめるの！塩をミヤコに振らないでなの～！成仏しちゃうの～！",
+            "アメが3つ揃ったの あめちゃんなの",
+            "ボウルとスプーンが3つ揃ったの これでプリンを作るの！3つ同時平行で大量生産なの～",
+            "お酒が3つ揃ったの お酒が入ったお風呂は身を清められるらしいの… や、やめてなの！ミヤコを突っ込まないで欲しいの！"
+            ]
+        for i in range(kaisuu):
+            A = random.choice(slot_list)
+            B = random.choice(slot_list)
+            C = random.choice(slot_list)
+            result = "%s%s%s" % (A, B, C)
+            if A == B and B == C:
+                await ctx.send(result + "\n" + result_list[slot_list.index(A)])
+            elif A == B or B == C or A == C:
+                await ctx.send(result + "\n" + "もうちょっとだったの！もう一回なの！")
+            else:
+                await ctx.send(result + "\n" + "はずれなの！もう一回なの！")
         
 def setup(bot):
     bot.add_cog(Casino(bot))
